682366d2cff30b00863409285e7521b45f46b8a7
zrygan <zr.gntn@gmail.com>
2026-01-21 11:56:30 -0500

    Organizing

--- src/adaptation/phonetic/p2g.rs
+++ src/adaptation/phonetic/p2g.rs
@@ -6,24 +6,34 @@ use crate::tokens;
 ///
 /// Returns (graphemes, is_diphthong) where is_diphthong indicates
 /// if this phoneme represents two sounds (and might consume two graphemes).
-pub fn graphemize(phoneme: &ArpabetSymbols) -> (Vec<FilipinoGrapheme>, bool) {
+pub fn graphemize(phoneme: &ArpabetSymbols) -> Option<(Vec<FilipinoGrapheme>, bool)> {
     match phoneme {
-        ArpabetSymbols::AA => (tokens![FilipinoGrapheme::A], false),
-        ArpabetSymbols::AE => (tokens![FilipinoGrapheme::A], false),
-        ArpabetSymbols::AH => (tokens![FilipinoGrapheme::A], false),
-        ArpabetSymbols::AO => (tokens![FilipinoGrapheme::O], false),
-        ArpabetSymbols::EH => (tokens![FilipinoGrapheme::E], false),
-        ArpabetSymbols::ER => (tokens![FilipinoGrapheme::E, FilipinoGrapheme::R], false),
-        ArpabetSymbols::IH => (tokens![FilipinoGrapheme::I], false),
-        ArpabetSymbols::IY => (tokens![FilipinoGrapheme::I], false),
-        ArpabetSymbols::UH => (tokens![FilipinoGrapheme::U], false),
-        ArpabetSymbols::UW => (tokens![FilipinoGrapheme::U], false),
-        ArpabetSymbols::AW => (tokens![FilipinoGrapheme::A, FilipinoGrapheme::W], true),
-        ArpabetSymbols::AY => (tokens![FilipinoGrapheme::A, FilipinoGrapheme::Y], true),
-        ArpabetSymbols::EY => (tokens![FilipinoGrapheme::E, FilipinoGrapheme::Y], true),
-        ArpabetSymbols::OW => (tokens![FilipinoGrapheme::O], true),
-        ArpabetSymbols::OY => (tokens![FilipinoGrapheme::O], true),
+        // for A
+        ArpabetSymbols::AA => Some((tokens![FilipinoGrapheme::A], false)),
+        ArpabetSymbols::AE => Some((tokens![FilipinoGrapheme::A], false)),
+        ArpabetSymbols::AH => Some((tokens![FilipinoGrapheme::A], false)),
+        ArpabetSymbols::AO => Some((tokens![FilipinoGrapheme::O], false)),
+        ArpabetSymbols::AY => Some((tokens![FilipinoGrapheme::A, FilipinoGrapheme::Y], true)),
+        ArpabetSymbols::AW => Some((tokens![FilipinoGrapheme::A, FilipinoGrapheme::W], true)),
 
-        _ => (tokens![FilipinoGrapheme::A], false),
+        // for E
+        ArpabetSymbols::EH => Some((tokens![FilipinoGrapheme::E], false)),
+        ArpabetSymbols::ER => Some((tokens![FilipinoGrapheme::E, FilipinoGrapheme::R], false)),
+
+        // for I
+        ArpabetSymbols::IH => Some((tokens![FilipinoGrapheme::I], false)),
+        ArpabetSymbols::IY => Some((tokens![FilipinoGrapheme::I], false)),
+
+        // for U
+        ArpabetSymbols::UW => Some((tokens![FilipinoGrapheme::U], false)),
+        ArpabetSymbols::UH => Some((tokens![FilipinoGrapheme::U], false)),
+
+        // for O
+        ArpabetSymbols::OW => Some((tokens![FilipinoGrapheme::O], true)),
+        ArpabetSymbols::OY => Some((tokens![FilipinoGrapheme::O], true)),
+
+        // for ai, ey (like bait)
+        ArpabetSymbols::EY => Some((tokens![FilipinoGrapheme::E, FilipinoGrapheme::Y], true)),
+        _ => None,
     }
 }
