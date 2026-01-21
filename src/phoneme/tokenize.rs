51c41f7d1672add15880840e34f9e7eb529579ab
zrygan <zr.gntn@gmail.com>
2026-01-21 11:33:53 -0500

    Improved naming convension and added ipa to arpa

--- src/phoneme/tokenize.rs
+++ src/phoneme/tokenize.rs
@@ -1,15 +1,19 @@
-use crate::phoneme::symbols::{ArpabetSymbols, match_arpabet};
+use crate::phoneme::{
+    map::IPA_TO_ARPA,
+    symbols::{ArpabetSymbols, match_arpabet},
+};
 
-/// Tokenize a string into arpabet symbols, matching longest patterns first
+/// Tokenize a string of arpabet symbols to a vector of arpabet symbols.
+/// Keyword: from string to a vector. Similar function: `tokenize_ipa`.
 ///
 /// # Arguments
 ///
-/// * `input` - The string to tokenize
+/// * `input` - Arpabet string (kwehschahn)
 ///
 /// # Returns
 ///
 /// Returns a vector of `ArpabetSymbols` enum values.
-pub fn tokenizer(input: &str) -> Vec<ArpabetSymbols> {
+pub fn tokenize_arpa(input: &str) -> Vec<ArpabetSymbols> {
     let chars: Vec<char> = input.chars().collect();
     let mut result = Vec::new();
     let mut i = 0;
@@ -19,7 +23,6 @@ pub fn tokenizer(input: &str) -> Vec<ArpabetSymbols> {
             i += 1;
             continue;
         }
-        // Check digraphs first (2 characters)
         if i + 2 <= chars.len() {
             let substring_2: String = chars[i..i + 2].iter().collect();
             if let Some(replacement) = match_arpabet(&substring_2) {
@@ -29,18 +32,50 @@ pub fn tokenizer(input: &str) -> Vec<ArpabetSymbols> {
             }
         }
 
-        // Fall back to single character
         let single_char: &str = &chars[i].to_string();
         if let Some(single_repl) = match_arpabet(single_char) {
             result.push(single_repl);
             i += 1;
         } else {
-            // Skip unknown characters (e.g., unknown phoneme outputs from G2P model)
             i += 1;
         }
     }
 
     result
+}
+
+/// Tokenize string of IPA symbols to a vector of arpabet symbols.
+/// Similar function: `tokenize_arpa`.
+///
+/// # Arguments
+/// * `ipa` - IPA string (e.g., "ækʃən")
+///
+/// # Returns
+/// Vector of ArpabetSymbols
+pub fn tokenize_ipa(ipa: &str) -> Vec<ArpabetSymbols> {
+    let mut tokens = Vec::new();
+    let chars: Vec<char> = ipa.chars().collect();
+    let mut i = 0;
+
+    while i < chars.len() {
+        if i + 1 < chars.len() {
+            let two_char: String = chars[i..=i + 1].iter().collect();
+            if let Some(arpa) = IPA_TO_ARPA.get(two_char.as_str()) {
+                tokens.push(arpa.clone());
+                i += 2;
+                continue;
+            }
+        }
+
+        let one_char: String = chars[i..=i].iter().collect();
+        if let Some(arpa) = IPA_TO_ARPA.get(one_char.as_str()) {
+            tokens.push(arpa.clone());
+        }
+
+        i += 1;
+    }
+
+    tokens
 }
 
 /// Convert a `Vec<SourceGrapheme>` back to a String
