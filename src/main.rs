2102aaa8ba188e1410ec7f14e6bb96c4b486c232
zrygan <zr.gntn@gmail.com>
2026-01-20 22:25:05 -0500

    Added the default workflow in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use tagabaybay::adaptation::adapter::Adapter;
 use tagabaybay::configs::AdaptationConfig;
 use tagabaybay::grapheme::filipino::graphemes_to_string;
+use tagabaybay::syllabification::algorithm::syllabify;
+use tagabaybay::grapheme::filipino::hyphenate;
 
 fn main() {
     let words = ["hello", "aspirin", "chocolate", "ibuprofen", "tetracycline"];
@@ -11,6 +13,10 @@ fn main() {
         match adapter.adaptation(word, &config) {
             Ok(result) => {
                 println!("{} -> {}", word, graphemes_to_string(&result));
+                if let Some((syll, validity)) = syllabify(&result) {
+                    let hyph = hyphenate(&syll);
+                    println!("syllabification: {hyph} || {validity}")
+                }
             }
             Err(e) => println!("{}: ERROR {:?}", word, e),
         }
